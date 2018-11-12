23e88b97ff818db8d36496cd555bdddd38b40f4c
Chris Fritz <chrisvfritz@gmail.com>
2018-11-12 17:28:57 -0500

    v2.0.0-alpha.2

--- demo/main.js
+++ demo/main.js
@@ -5,8 +5,8 @@ if (process.env.VUE_APP_E2E) {
   if (window.__e2e_lib) {
     Vue.use(require('hello-vue-components').default)
   } else if (window.__e2e_components) {
-    Vue.component('HelloA', require('hello-vue-components.HelloA'))
-    Vue.component('HelloB', require('hello-vue-components.HelloB'))
+    Vue.component('HelloA', require('hello-vue-components.hello-a').default)
+    Vue.component('HelloB', require('hello-vue-components.hello-b').default)
   }
 } else {
   // Install the plugin directly from src in development
